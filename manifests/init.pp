3e336e033324aec9d2c66b905179e28f2fbe03c7
Christophe Laffont <claffont@openstudio.fr>
2014-02-27 10:48:12 +0100

    ajout installe.pp

--- manifests/init.pp
+++ manifests/init.pp
@@ -15,22 +15,7 @@
 
 class thelia {
   
-  # Clone Thelia
-  vcsrepo { "/var/www/${::hostname}":
-    ensure   => present,
-    provider => git,
-    source   => 'https://github.com/thelia/thelia.git',
-    revision => 'master',
-    user     => 'vagrant'
-  }
-
-  # Install
-  exec { 'run composer for installing dependencies':
-    command   => "composer install",
-    cwd => '/var/www/${::hostname}',
-    timeout => 0,
-    tries => 10
-  }
+  include thelia::install
 
 }
 
---
+++ manifests/install.pp
@@ -0,0 +1,35 @@
+# Class: thelia
+#
+# This module manages thelia
+# http/www.thelia.net
+#
+# Parameters:
+# - None
+#
+#
+# Author:
+# Christophe Laffont
+#
+
+class thelia::install {
+  
+  # Clone Thelia
+  vcsrepo { "/var/www/thelia.dev":
+    ensure   => present,
+    provider => git,
+    source   => 'https://github.com/thelia/thelia.git',
+    revision => 'master',
+    user     => 'vagrant'
+  }
+
+  # Install
+  exec { 'run composer for installing dependencies':
+    command   => "composer install",
+    cwd => '/var/www/thelia.dev',
+    timeout => 0,
+    tries => 10,
+    require => Package['composer']
+  }
+
+}
+
