936782424f312952ec358c71202bca9c65ed1732
Christophe Laffont <claffont@openstudio.fr>
2014-02-27 11:54:30 +0100

    add force true

--- manifests/install.pp
+++ manifests/install.pp
@@ -14,17 +14,18 @@
 class thelia::install {
 
   # Clone Thelia
-  vcsrepo { "/var/www/thelia":
+  vcsrepo { "/var/www/thelia.dev":
     ensure   => latest,
     provider => git,
-    source   => 'git://github.com/thelia/thelia.git',
+    source   => 'https://github.com/thelia/thelia.git',
     user     => 'vagrant',
+    force => true
   }
 
   # Install
   exec { "run composer for installing dependencies":
     command   => "composer install",
-    cwd       => '/var/www/thelia',
+    cwd       => '/var/www/thelia.dev',
     require   => Class['composer'],
     timeout   => 600
   }
